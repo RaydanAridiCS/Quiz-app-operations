bc5b75115ae670b84fce790d1d03fef3441caf38
RaydanAridiCS <RaydanAridi.CS@gmail.com>
2025-04-24 17:38:32 +0300

    Added Get all Quizzes API

---
+++ get_quizzes.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'connection.php';
+
+$sql = "SELECT * FROM Quizzes";
+
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $quizzes[] = array(
+            'QuizID' => $row['QuizID'],
+            'Title' => $row['Title'],
+            'Description' => $row['Description'],
+            'CreatedBy' => $row['CreatedBy'],
+            'IsPublished' => $row['IsPublished']
+        );
+    }
+    echo json_encode($quizzes);
+} else {
+    echo json_encode(array(
+            'error'=> 'No quizzes found'
+        ));
+}
+
+mysqli_close($conn);
+
+?>
