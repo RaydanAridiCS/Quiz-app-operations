5ec4b1692d7be491810e4990db83c1b904778400
RaydanAridiCS <RaydanAridi.CS@gmail.com>
2025-04-24 17:57:22 +0300

    Organized all user APIs in 1 folder

--- user/create_user.php
+++ user/create_user.php
@@ -1,5 +1,5 @@
 <?php
-require 'connection.php';
+require '../connection.php';
 
 
 $new_username = "newUser"; 
--- user/login_user.php
+++ user/login_user.php
@@ -1,5 +1,5 @@
 <?php
-require 'connection.php';
+require '../connection.php';
 
 $username = "newUser"; 
 $email = "test@gmail.com";
